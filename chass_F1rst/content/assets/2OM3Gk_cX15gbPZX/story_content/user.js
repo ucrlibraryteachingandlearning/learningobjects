41577dbc6c73695e4dfd950963eb7a6d5f057a6d
ucrlibraryteachingandlearning <55596707+ucrlibraryteachingandlearning@users.noreply.github.com>
2022-08-11 13:30:11 -0700

    Updated 8-11-22
    Updated with accessibility improvements and Get it at UC video

---
+++ chass_F1rst/content/assets/2OM3Gk_cX15gbPZX/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6O4XRkXfvLm":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("Complete");
+}
+
